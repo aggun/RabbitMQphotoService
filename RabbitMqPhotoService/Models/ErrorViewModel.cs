b5228699affe770936464b8104da8aa5ed5f15a3
Nebil Aggun <nebil.aggun@gmail.com>
2022-08-10 10:01:49 +0300

    completed Project push the Github

---
+++ RabbitMqPhotoService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RabbitMqPhotoService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ RabbitMqPhotoService/Service/productImageCreatedEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace RabbitMqPhotoService.Service
+{
+    public class productImageCreatedEvent
+    {
+        public string ImageName { get; set; }
+    }
+}
